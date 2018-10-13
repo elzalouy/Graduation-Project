72c967685912e3eb34af20e0f50b0731d2132f00
Ezat Elzalouy <ezatelzalouy711@gmail.com>
2018-10-13 23:52:12 +0200

    UserAuthenticationApp
    This is a the initial project with ASP-NET users and roles in a table in a database

---
+++ Life/Life/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Life.Startup))]
+namespace Life
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
